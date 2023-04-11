bdb67b8b3304c5912f30dbae45143c5144d7f52b
Arthur <arthur.albert990@gmail.com>
2023-04-11 14:13:42 -0300

    Add Tailwind support

--- cv-project/src/index.js
+++ cv-project/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
+import PersonalInfo from './Components/PersonalInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <PersonalInfo/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+//INÍCIO 11/04, 11:25 ÀS 12:00
+//INSTALANDO TAILWIND 13:06 às 14:00 Tailwind funcionando, precisou reiniciar o VS Code.
+//
---
+++ cv-project/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/Components/*.js",
+    "./App.js",
+    "./index.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
