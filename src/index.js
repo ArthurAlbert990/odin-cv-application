9af7136ea11a3c1729139aa5279c8380a94fb4c4
Arthur <arthur.albert990@gmail.com>
2023-04-18 09:40:01 -0300

    Start general styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/styles.css'
 import App from './App.js';
 import PersonalInfo from './Components/PersonalInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <section className='w-screen h-auto bg-teal-400 flex items-center justify-center'>
+      <h1 className='text-2xl font-medium text-slate-50 p-2'>CV Application</h1>
+    </section>
     <App />
     <PersonalInfo/>
   </React.StrictMode>
@@ -18,6 +22,9 @@ root.render(
 
 
 
-//INÍCIO 11/04, 11:25 ÀS 12:00
-//INSTALANDO TAILWIND 13:06 às 14:00 Tailwind funcionando, precisou reiniciar o VS Code.
+//INÍCIO 11/04:
+// 11:25 ÀS 12:00 INSTALANDO TAILWIND 
+// 13:06 às 14:00 Tailwind funcionando, precisou reiniciar o VS Code.
+//18/04:
+// 09:00 ÀS 09:38 // tailwind, estilizando página.
 //
